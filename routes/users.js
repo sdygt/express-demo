856df976a2d3801d7074493e021658b259100fb7
sdygt <root@sdygt.net>
2018-01-02 23:48:28 +0800

    RESTful API Example

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).send(JSON.stringify(router.locals.todolist));
 });
 
 module.exports = router;
